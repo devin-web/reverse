6d3ca46037b1ba56edd1eb7e41563a64312bf24d
devin-web <devin@alumni.sfu.ca>
2016-09-06 02:40:01 +0000

    reverse strings not numbers

---
+++ reverse.js
@@ -0,0 +1,36 @@
+function readCmdLine(){
+  var myArgs = process.argv.slice(2);
+  return myArgs;
+}
+ //console.log('myArgs: ', myArgs);
+var input = readCmdLine();
+reverse( input );
+
+function isNotANumber( inputString ){
+  var returnVal = isNaN(inputString);
+  return returnVal;
+}
+
+function reverse( inputStrings ){
+  var reversedString = "";
+  for( var i = 0; i < inputStrings.length; i++ ){
+    if( isNotANumber(inputStrings[i] )){
+      reversedString = reverseString( inputStrings[i] );
+    }
+    else {
+      reversedString = inputStrings[i]; //non reversed number
+    }
+    console.log( reversedString );
+  }
+}
+
+function reverseString( inputString ){
+  var reversedString  = "";
+
+  for( var i = inputString.length-1; i >= 0; i--){
+    reversedString += inputString[i];
+  }
+  //console.log( "reversed string: ", reversedString );
+  return reversedString;
+
+}
